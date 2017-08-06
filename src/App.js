b6a1829d8a673c1c1422b57e1930a0191a68858c
DeMarc Johnson <demarcjohnson@yahoo.com>
2017-08-05 20:46:28 -0400

    Grabing recent updates

--- src/App.js
+++ src/App.js
@@ -24,12 +24,26 @@ class App extends Component {
     evt.preventDefault();
     const tournamentId = SearchTournament(this.state.currentSearch);
     const playerId = SearchPlayer(this.state.player1Results);
-    console.log(tournamentId);
-    loadBracket([tournamentId])
-      .then(response => this.setState({
-        searchResults: response.entities.seeds[0].mutations.participants[playerId].gamerTag
-      }))
-      .then(console.log("It works!"));
+    this.setState({
+      searchResults: playerId
+    })
+    // loadBracket([tournamentId])
+    //   .then(response => {
+    //     let playerList = response.entities.seeds.map((obj) => Object.keys(obj.mutations.participants));
+    //     response.entities.seeds.map((obj, i) => {
+    //       let valPlayer = obj.mutations.participants[playerList[i]].gamerTag;
+    //       console.log(this.state.player1Results);
+    //       if(this.state.player1Results === valPlayer){
+    //         console.log("This Works!");
+    //         return this.setState({searchResults: this.state.player1Results});
+    //       } else {
+    //         this.setState({
+    //           searchResults: "Player not found!"
+    //         });
+    //       }
+    //       // return this.state.searchResults;
+    //     })  
+    //   }); 
   }
 
   handlePlayerName = (evt) => {
--- src/lib/searchPlayer.js
+++ src/lib/searchPlayer.js
@@ -1,7 +1,23 @@
-export const SearchPlayer = (playerName) => {
-  let playerId = 0;
-  if(playerName === "axe"){
-    playerId = 822516;
-  }
-  return playerId;
+import {loadBracket} from "./bracketService"
+
+  
+
+export const SearchPlayer = async function(playerName) {
+  let getId = "Player not found!";
+  loadBracket(335886)
+    .then(response => {
+      let playerList = response.entities.seeds.map((obj) => Object.keys(obj.mutations.participants));
+      response.entities.seeds.map(function(obj, i) {
+        let valPlayer = obj.mutations.participants[playerList[i]].gamerTag;
+        if(valPlayer === playerName){
+          getId = valPlayer;
+          console.log(getId);
+          return valPlayer
+        } else {
+          return getId;
+        }
+      })
+    })
+  return getId;
 }
+
