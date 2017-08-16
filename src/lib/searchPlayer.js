1d5392ba08b1157ee8a47bf5d7806a4ac08715a4
DeMarc Johnson <demarcjohnson@yahoo.com>
2017-08-15 23:48:14 -0400

    Changed code in searchPlayer.js to use more functional/expression programming and added es8 code to it too!

--- src/lib/searchPlayer.js
+++ src/lib/searchPlayer.js
@@ -1,36 +1,26 @@
 import { loadBracket, checkPlayerName, checkTournamentName } from "./index";
 
-let pageNum = 1;
-
 export const SearchPlayer = (userInputTournamentName, userInputPlayerName, maxPageNum) => {
   // function pageLoop() {
-  //   i++
-    // console.log("Loop iterable", i);
+    let pageNum = 1;
     const tournamentName = checkTournamentName(userInputTournamentName)
-    let bracket = loadBracket(tournamentName, 1)
+    let bracket = loadBracket(tournamentName, pageNum)
       .then(res => {
-           pageNum++
-          const playerName = res.items.entities.entrants.map(obj => {
-            return Object.keys(obj.mutations.participants);
-          })
-          .map((key, i) => {
-            console.log("playerNameList", res.items.entities.entrants[i].mutations.participants[key].gamerTag);
-            return res.items.entities.entrants[i].mutations.participants[key].gamerTag;
-          })
-          .filter( name => {
+          pageNum++
+          const playerName = res.items.entities.entrants.filter(obj => {
+            const name = Object.values(obj.mutations.participants)[0].gamerTag;
             return checkPlayerName(name) === checkPlayerName(userInputPlayerName);
-          });
+          })
+          .map(name => Object.values(name.mutations.participants)[0].gamerTag)
 
-          if(playerName.length === 0){
-            if(pageNum >= maxPageNum){
-              return "Player Not Found!";
-            }
-            return bracket;
-            } else {
-              return playerName[0];
-          }
+          const result = (playerName.length === 0)
+            ? (pageNum >= maxPageNum)
+              ? "Player Not Found!"
+              : bracket
+            : playerName[0]
+
+          return result;
       })
-
     return bracket;
     // }
   // return pageLoop();
