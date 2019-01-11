d5030ff12981b447a3472326dbeb48b8791ca0ec
DeMarc Johnson <demarcjohnson@gmail.com>
2019-01-11 18:33:17 -0500

    Fixed the api changes for smash.gg and got rid of the CORS heroku app

--- src/lib/getGameEvent.js
+++ src/lib/getGameEvent.js
@@ -1,17 +1,14 @@
 import { checkTournamentName } from "./index";
 
 export const getGame = async (phaseGroupId) => {
-  async function getPlayerName () {
     let tournament = checkTournamentName(phaseGroupId);
     const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
     const targetUrl = `https://api.smash.gg/tournament/${tournament}?expand[]=event&expand[]=phase`;
     const getUrl = await fetch(proxyUrl + targetUrl);
     const getJson = await getUrl.json();
-    const gameTypeId = await Object.values(getJson.entities.event);
-    const gameType = await gameTypeId.map(game => game.name);
+    const gameTypeId = Object.values(getJson.entities.event);
+    const gameType = gameTypeId.map(game => game.name);
     gameType.unshift("Select Game");
     return gameType;
-  }
-  return getPlayerName();
 }
   
