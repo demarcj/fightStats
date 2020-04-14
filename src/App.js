dd32e6a4a07eaf2a195d18d0d43c32a0b57dbbe6
DeMarc Johnson <demarcjohnson@gmail.com>
2020-04-14 19:35:57 -0400

    Switch from css to sass, did a little of styling/design, and added typescript haven't used it yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './App.scss';
 import { SearchForm, SearchFormTournament } from "./components/ui-form";
 import { search_tournament, test_error, get_game, check_game } from "./lib";
 
@@ -49,29 +49,42 @@ export default function App () {
   const submit_search = tournament ? search_submit : empty_submit;
   return (
     <>
-      <div className="App-header">
-        <h2>Welcome to Fighter Stats</h2>
-      </div>
-      <p className="App-intro">Select the Tournament</p>
-      <p>{error}</p>
-      <SearchFormTournament
-        input_change={input_change}
-        tournament_submit={tournament_submit}
-        event_list={event_list}
-        tournament={tournament}
-        game={game} 
-      />
-      <SearchForm
-        input_change={input_change}
-        player1={player1}
-        handle_submit={submit_search}
-      />
-      <div className="text_container">
-        <p className="body_text">To test the form go to <a href="https://smash.gg">smash.gg</a> and copy and paste the name of the tournament in the first</p>
-        <p className="body_text">Select a game</p>
-        <p className="body_text">Type a player's name in the Player field to see if that person attended the tournament</p>
-        <p className="body_text">{results} <a href="https://smash.gg">{helper_message}</a></p>
-      </div>
+      <header>
+        <aside></aside>
+        <h2 className="App-header">Welcome to Fighter Stats</h2>
+        <aside></aside>
+      </header>
+      <main className="main">
+        <aside></aside>
+        <section class="content">
+          <p className="App-intro">Select the Tournament</p>
+          <p className="errot_text">{error}</p>
+          <SearchFormTournament
+            input_change={input_change}
+            tournament_submit={tournament_submit}
+            event_list={event_list}
+            tournament={tournament}
+            game={game} 
+          />
+          <SearchForm
+            input_change={input_change}
+            player1={player1}
+            handle_submit={submit_search}
+          />
+          <div className="text_container">
+            <p className="body_text">1. To test the form go to <a href="https://smash.gg">smash.gg</a> and copy and paste the name of the tournament in the first</p>
+            <p className="body_text">2. Select a game</p>
+            <p className="body_text">3. Type a player's name in the Player field to see if that person attended the tournament</p>
+            <p className="body_text">{results} <a href="https://smash.gg">{helper_message}</a></p>
+          </div>
+        </section>
+        <aside></aside>
+      </main>
+      <footer className="footer">
+        <aside></aside>
+        <p className="copyright">&copy; 2020 DeMarc Johnson</p>
+        <aside></aside>
+      </footer>
     </>
   );
 }
